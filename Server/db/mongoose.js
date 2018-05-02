81a2c58c771c452fb64f98dcf4c1eaa2d0418c86
Naidu <eshwar.naidu@accenture.com>
2018-05-02 20:47:16 +0530

    small changes

--- Server/db/mongoose.js
+++ Server/db/mongoose.js
@@ -1,7 +1,11 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://eshwar:eshwar@ds211440.mlab.com:11440/node-todo' || 'mongodb://localhost:27017/TodoApp');
+
+var username = 'eshwar';
+var password = 'eshwar';
+
+mongoose.connect(`mongodb://${username}:${password}@ds211440.mlab.com:11440/node-todo` || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {
     mongoose
