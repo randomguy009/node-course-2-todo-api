91b2973a6108f34c917debfbf71ff12246e8c5c9
Naidu <eshwar.naidu@accenture.com>
2018-05-02 13:28:20 +0530

    commiting additional changes

--- Server/server.js
+++ Server/server.js
@@ -5,6 +5,8 @@ var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
+const {ObjectID} = require('mongodb');
+
 var app = express();
 
 app.use(bodyParser.json());
@@ -29,6 +31,25 @@ app.get('/todos', (req, res) => {
     }).catch((e) => {
         res.status(400).send(e);
     }); 
+});
+
+app.get('/todos/:id', (req, res) => {
+    //res.send(req.params);
+    var id = req.params.id;
+
+    if (!ObjectID.isValid(id)) {
+        res.status(404).send();
+    }
+    else {
+        Todo.findById(id).then((todo) => {
+            if(!todo) {
+                return res.status(404).send();
+            }
+            res.send(todo);
+        }).catch((e) => {
+            res.status(400).send();
+        });
+    }
 });
 
 app.listen(3000, () => {
